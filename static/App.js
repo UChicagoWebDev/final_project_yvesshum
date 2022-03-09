4221c1313770ebd12c7255cd6aa686d6cda6a2fa
Yves <yvesshum1210@gmail.com>
2022-03-08 22:23:01 -0600

    basic setup

---
+++ static/App.js
@@ -0,0 +1,11 @@
+import Login from "./components/Login.js";
+function App() {
+    return (
+        <div>
+            <div>Hello World</div>
+            <Login />h
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
