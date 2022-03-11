b145ebbd632f10a6f657a9e819936dce687f298c
Yves Shum <yvesshum1210@gmail.com>
2022-03-10 18:27:02 -0600

    changed to create_react_app

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template, request, jsonify, send_from_directory, Blueprint
 from functools import wraps
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='react-app/build')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/api/test')
+def test():
+    return "Ok"
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
